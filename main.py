af97de05c906a45c5f3050e4d8fae34d9bf0efff
Mika Koivisto <mika.koivisto@hotmail.com>
2026-03-19 22:44:25 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import io
+import os
+from traceback import print_exc
+
+import pandas as pd
+from urllib.parse import quote
+
+import psycopg2
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_bbox(city):
+    osm_url = f"https://nominatim.openstreetmap.org/search?q={quote(city)}&format=json"
+    headers = {'User-Agent': 'OpenAQCityBBox'}
+
+    response = requests.get(osm_url, headers=headers).json()
+
+    if not response:
+        return None
+
+    # boundingbox sisältää löydetyn kaupungin rajat
+    # siinä on 4 koordinaattipisettä
+    osm_bbox = response[0]['boundingbox']
+
+    # OpenStreetMapin bounding boxin koordinaatit ovat ao järjestyksessä
+    # min_y, max_y, min_x, max_x
+    min_lat, max_lat, min_lon, max_lon = osm_bbox
+
+    # järjestetään uudelleen openAQ:lle sopivaan muotoon: min_x, min_y, max_x, max_y
+    openaq_bbox = f"{min_lon},{min_lat},{max_lon},{max_lat}"
+
+    return openaq_bbox
+
+# tämä funktio saa parametrinaan kaupungin bounding boxin get_bbox-funktiolta
+def get_openaq_locations_by_bbox(_bbox):
+    response = requests.get(
+        f'https://api.openaq.org/v3/locations?limit=1000&page=1&order_by=id&sort_order=asc&bbox={_bbox}',
+        headers={'X-API-Key': os.getenv('API_KEY')})
+    _locations = []
+    # muista, että http-statuskoodi 200 on OK
+    # voit myös heittää poikkeuksen,
+    # jos statuskoodi on jotakin muuta kuin 200
+    if response.status_code == 200:
+        _locations = response.json()['results']
+
+    return _locations
+
+def download_file_by_location(location_id, year, month):
+    base_url = "https://openaq-data-archive.s3.amazonaws.com"
+    for day in range(1,32):
+        date_str = f"{year}{month:02d}{day:02d}"
+        key = f"records/csv.gz/locationid={location_id}/year={year}/month={month:02d}/location-{location_id}-{date_str}.csv.gz"
+        full_url = f"{base_url}/{key}"
+
+        # 2. Use requests to get the file
+        response = requests.get(full_url)
+
+        if response.status_code == 200:
+            # pandas osaa avata gzip-pakatun csv
+            df = pd.read_csv(io.BytesIO(response.content), compression='gzip')
+            df.to_csv(f"{location_id}-{date_str}.csv", index=False)
+        else:
+            print(f"Failed to fetch. Status: {response.status_code}")
+
+if __name__ == "__main__":
+    bbox = get_bbox("Helsinki")
+    _locations = get_openaq_locations_by_bbox(bbox)
+    #download_file_by_location(2975, 2026, 1)
+
+
