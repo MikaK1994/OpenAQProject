47eaddaada47cfe679045982fa4d206e0716473b
Mika Koivisto <mika.koivisto@hotmail.com>
2026-03-22 17:52:58 +0200

    added run(), changed for loop for downloading csv files to use calendar.monthrange and added folder "data" for csv files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import io
 import os
 import traceback
 from traceback import print_exc
-
+import calendar
 import pandas as pd
 from urllib.parse import quote
 
@@ -51,7 +51,8 @@ def get_openaq_locations_by_bbox(_bbox):
 
 def download_file_by_location(location_id, year, month):
     base_url = "https://openaq-data-archive.s3.amazonaws.com"
-    for day in range(1,32):
+    num_days = calendar.monthrange(year, month)[1]
+    for day in range(1, num_days +1):
         date_str = f"{year}{month:02d}{day:02d}"
         key = f"records/csv.gz/locationid={location_id}/year={year}/month={month:02d}/location-{location_id}-{date_str}.csv.gz"
         full_url = f"{base_url}/{key}"
@@ -62,14 +63,16 @@ def download_file_by_location(location_id, year, month):
         if response.status_code == 200:
             # pandas osaa avata gzip-pakatun csv
             df = pd.read_csv(io.BytesIO(response.content), compression='gzip')
-            df.to_csv(f"{location_id}-{date_str}.csv", index=False)
+            df.to_csv(f"data/{location_id}-{date_str}.csv", index=False)
         else:
             print(f"Failed to fetch. Status: {response.status_code}")
 
 def _populate_countries():
     with psycopg2.connect(dbname=os.getenv('DB'), user=os.getenv('DB_USER'), password=os.getenv('DB_PWD')) as conn:
         with conn.cursor() as cur:
-
+            cur.execute("DELETE FROM measurements;")
+            cur.execute("DELETE FROM locations;")
+            cur.execute("DELETE FROM cities;")
             cur.execute("DELETE FROM countries;")
             conn.commit()
 
@@ -79,6 +82,7 @@ def _populate_countries():
                 for key, (code, name) in countries.items():
                     cur.execute(_query,(key, code, name))
                 conn.commit()
+                print("Maat lisätty tietokantaan")
             except Exception as e:
                 conn.rollback()
                 print_exc()
@@ -86,7 +90,8 @@ def _populate_countries():
 def _populate_cities():
     with psycopg2.connect(dbname=os.getenv('DB'), user=os.getenv('DB_USER'), password=os.getenv('DB_PWD')) as conn:
         with conn.cursor() as cur:
-            cur.execute('DELETE FROM locations;')
+            cur.execute("DELETE FROM measurements;")
+            cur.execute("DELETE FROM locations;")
             cur.execute("DELETE FROM cities;")
             conn.commit()
 
@@ -96,6 +101,7 @@ def _populate_cities():
                 for key, (name, country_id) in cities.items():
                     cur.execute(_query,(key, name, country_id))
                 conn.commit()
+                print("Kaupungit lisätty tietokantaan")
             except Exception as e:
                 conn.rollback()
                 print_exc()
@@ -119,6 +125,7 @@ def _populate_locations():
                     cur.execute(_query, (row['location_id'], row['lat'],
                                 row['lon'], row['location'][:-5], row['city_id']))
                 conn.commit()
+                print("Paikkakunnat lisätty tietokantaan")
             except Exception as e:
                 conn.rollback()
                 traceback.print_exc()
@@ -140,6 +147,7 @@ def _populate_sensors():
                 for _index, row in df.iterrows():
                     cur.execute(_query, (row['sensors_id'], row['parameter'], row['units']))
                 conn.commit()
+                print("Sensorit lisätty tietokantaan")
             except Exception as e:
                 conn.rollback()
                 traceback.print_exc()
@@ -153,22 +161,60 @@ def _populate_measurements():
     _query = 'INSERT INTO measurements(datetime, value, location_id, sensors_id) VALUES (%s, %s, %s, %s);'
     with psycopg2.connect(dbname=os.getenv('DB'), user=os.getenv('DB_USER'), password=os.getenv('DB_PWD')) as conn:
         with conn.cursor() as cur:
-            cur.execute("DELETE FROM measurements")
+            cur.execute("TRUNCATE TABLE measurements RESTART IDENTITY;")
             conn.commit()
             try:
                 for _index, row in df.iterrows():
                     cur.execute(_query, (row['datetime'], row['value'],
                                         row['location_id'], row['sensors_id']))
                 conn.commit()
+                print("Mittaustulokset lisätty tietokantaan")
             except Exception as e:
                 conn.rollback()
                 traceback.print_exc()
 
+def run():
+    # ikiluuppi pyörii koko ajan, jos ei käyttäjä katkaise suoritusta valitsemalla 0
+    while True:
+        _choice = input(
+            "Valitse, mitä haluat tehdä:  "
+            "\n(0: Lopeta, "
+            "\n1: Lataa kuukauden ajalta mittausdatat csv:nä   "
+            "\n2: Vie maat tietokantaan   "
+            "\n3: Vie kaupungit tietokantaan   "
+            "\n4: Vie paikkakunnat tietokantaan   "
+            "\n5: Vie sensorit tietokantaan    "
+            "\n6: Vie mittaustulokset tietokantaan    "
+        )
+
+        # jos käyttäjä valitsee 0, ohjelman suoritus lopetetaan
+        if _choice == "0":
+            break
+        # jos valitaan 1
+        elif _choice == "1":
+            # kysytään vuosi
+            year = input("Anna vuosi:  ")
+            # ja kuukausi
+            month = input("Anna kuukausi:  ")
+            location_id= 2975
+            download_file_by_location(int(location_id),int(year), int(month))
+        elif _choice == "2":
+            _populate_countries()
+        elif _choice == "3":
+            _populate_cities()
+        elif _choice == "4":
+            _populate_locations()
+        elif _choice == "5":
+            _populate_sensors()
+        elif _choice == "6":
+            _populate_measurements()
+
+    print()
+    print("done")
+
 if __name__ == "__main__":
     bbox = get_bbox("Helsinki")
-    _locations = get_openaq_locations_by_bbox(bbox)
-    #download_file_by_location(2975, 2026, 1)
-    _populate_measurements()
+    run()
 
 
 
